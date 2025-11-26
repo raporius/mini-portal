cb6f49156cbc01fe9158a826467bd780ef2ae441
Raphael Destruhaut <rdestruh@c1r2p11.42lehavre.fr>
2025-11-26 14:50:04 +0100

    Cleanup of obj files and the library.

--- libft-portal/srcs/other/ft_atol.c
+++ libft-portal/srcs/other/ft_atol.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atol.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: macolomi <macolomi@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rdestruh <rdestruh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/18 13:24:16 by macolomi          #+#    #+#             */
-/*   Updated: 2025/11/26 12:42:53 by macolomi         ###   ########.fr       */
+/*   Updated: 2025/11/26 14:44:23 by rdestruh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,35 +34,7 @@ long	ft_atol(const char *str)
 		nbr = nbr * 10 + (str[i] - '0');
 		i++;
 	}
-	if (nbr > INT_MAX || nbr < 0)
-		return (LONG_MAX);
 	if (negative)
 		nbr = nbr * -1;
 	return (nbr);
-}
-
-int	ft_atol_p(const char *str, long *nbr)
-{
-	size_t		i;
-	bool		negative;
-
-	negative = 0;
-	*nbr = 0;
-	i = 0;
-	while (str[i] == ' ' || (str[i] >= '\t' && str[i] <= '\r'))
-		i++;
-	if (str[i] == '+' || str[i] == '-')
-	{
-		if (str[i] == '-')
-			negative = 1;
-		i++;
-	}
-	while (str[i] >= '0' && str[i] <= '9')
-	{
-		*nbr = *nbr * 10 + (str[i] - '0');
-		i++;
-	}
-	if (negative)
-		*nbr = *nbr * -1;
-	return (i);
 }
