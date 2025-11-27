e2f904ca3c18f8677746c19ae09cff3e8bdc2aa0
Celen42 <mateocolomines@gmail.com>
2025-11-27 16:11:48 +0100

    add of ascii art

--- includes/miniportal_celen.h
+++ includes/miniportal_celen.h
@@ -6,7 +6,7 @@
 /*   By: celen <celen@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/27 14:20:32 by celen             #+#    #+#             */
-/*   Updated: 2025/11/27 14:55:34 by celen            ###   ########.fr       */
+/*   Updated: 2025/11/27 15:45:44 by celen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,6 @@
 # define MINIPORTAL_CELEN_H
 
 # include "libft.h"
+# include "portal.h"
 
 #endif
