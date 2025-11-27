98f55f6f14ccaedc66d6390cf25493ee244a8498
Raphael Destruhaut <rdestruh@c1r2p12.42lehavre.fr>
2025-11-27 16:06:36 +0100

    Added basic structures.

---
+++ includes/structs.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   structs.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rdestruh <rdestruh@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/27 15:03:15 by rdestruh          #+#    #+#             */
+/*   Updated: 2025/11/27 16:05:15 by rdestruh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef STRUCTS_H
+# define STRUCTS_H
+
+typedef struct s_envp
+{
+	char			*name;
+	char			*val;
+	struct s_envp	*next;
+}	t_envp;
+
+typedef struct s_cmds
+{
+	char			**argv;
+	t_envp			*envp;
+	char			*infile;
+	char			*outfile;
+	bool			is_builtin;
+	struct s_cmds	*next;
+}	t_cmds;
+
+#endif
