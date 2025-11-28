8e844d20299657f4b07df4e705ab8551a5a192fe
Raphael Destruhaut <rdestruh@c1r1p9.42lehavre.fr>
2025-11-28 14:35:21 +0100

    Added envp management funcs in header file

---
+++ includes/miniportal_rapo.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   miniportal_rapo.h                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rdestruh <rdestruh@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/28 10:39:09 by rdestruh          #+#    #+#             */
+/*   Updated: 2025/11/28 14:30:12 by rdestruh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINIPORTAL_RAPO_H
+# define MINIPORTAL_RAPO_H
+
+# include "libft.h"
+# include "portal.h"
+# include "structs.h"
+
+//t_envp funcs
+t_envp	*envnode_new(char *name, char *val);
+void	envnode_add(t_envp **envp, t_envp *new);
+void	envnode_remove_by_name(t_envp **envp, char *to_remove);
+void	envp_clean(t_envp **envp);
+
+//envp manipulation
+t_envp	*init_envp(char **envp);
+
+#endif
